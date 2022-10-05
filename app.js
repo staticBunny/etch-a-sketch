9423fcb1b2b63f3b9136b8b14efa0ffaa9319ba5
staticBunny <fatehbir2000@gmail.com>
2022-10-05 16:47:27 +0530

    Add option for custom grid size

--- app.js
+++ app.js
@@ -1,34 +1,55 @@
 const container = document.querySelector('#container');
+const gridSizeInput = document.querySelector('#input');
 const RESET_BUTTON = document.querySelector('#reset');
 
 const ROW_CLASS = 'row';
 const GRID_SQUARE_CLASS = 'gridSquare';
 const ACTIVE_CLASS = 'active';
 
-function makeGrid(container, rowClass, gridSquareClass) {
-    for (let i = 0; i < 16; i++) {
+function makeGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
         const rowDiv = document.createElement('div');
-        rowDiv.classList.add(rowClass);
-        for (let j = 0; j < 16; j++) {
+        rowDiv.classList.add(ROW_CLASS);
+        for (let j = 0; j < gridSize; j++) {
             const squareDiv = document.createElement('div');
-            squareDiv.classList.add(gridSquareClass);
+            squareDiv.classList.add(GRID_SQUARE_CLASS);
             rowDiv.appendChild(squareDiv);
         }
         container.appendChild(rowDiv);
     }
+    changeSquareColor();
 }
 
-function changeSquareColor(gridSquareClass, activeClass) {
-    const gridSquares = document.querySelectorAll(`.${gridSquareClass}`);
+function deleteGrid() {
+    let child = container.firstElementChild;
+    while(child) {
+        child.remove();
+        child = container.firstElementChild;
+    }
+}
+
+function changeSquareColor() {
+    const gridSquares = document.querySelectorAll(`.${GRID_SQUARE_CLASS}`);
     gridSquares.forEach(square => square.addEventListener('mouseover', () => {
-        square.classList.add(activeClass);
+        square.classList.add(ACTIVE_CLASS);
     }))
 }
 
-function resetGrid(resetButton) {
-    resetButton.addEventListener('click', () => window.location.reload());
+function resetGrid() {
+    RESET_BUTTON.addEventListener('click', () => window.location.reload());
 }
 
-makeGrid(container, ROW_CLASS, GRID_SQUARE_CLASS);
-changeSquareColor(GRID_SQUARE_CLASS, ACTIVE_CLASS);
-resetGrid(RESET_BUTTON);
+function changeGrid() {
+    gridSizeInput.addEventListener('keydown', e => {
+        if (e.key === 'Enter'){
+            let value = gridSizeInput.value;
+            if (value > 101 || value < 1) return;
+            deleteGrid();
+            makeGrid(value);
+        } 
+    })
+}
+
+makeGrid(16);
+changeGrid();
+resetGrid();

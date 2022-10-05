e410ff0efd7de36566ba526cfd182b8f139dbf48
staticBunny <fatehbir2000@gmail.com>
2022-10-05 14:41:41 +0530

    Add 'hover' effect on grid squares

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const container = document.querySelector('#container');
 const ROW_CLASS = 'row';
 const GRID_SQUARE_CLASS = 'gridSquare';
+const ACTIVE_CLASS = 'active';
 
-function makeGrid(container) {
+function makeGrid(container, rowClass, gridSquareClass) {
     for (let i = 0; i < 16; i++) {
         const rowDiv = document.createElement('div');
-        rowDiv.classList.add(ROW_CLASS);
+        rowDiv.classList.add(rowClass);
         for (let j = 0; j < 16; j++) {
             const squareDiv = document.createElement('div');
-            squareDiv.classList.add(GRID_SQUARE_CLASS);
+            squareDiv.classList.add(gridSquareClass);
             rowDiv.appendChild(squareDiv);
         }
         container.appendChild(rowDiv);
     }
 }
 
-makeGrid(container);
+function changeSquareColor(gridSquareClass, activeClass) {
+    const gridSquares = document.querySelectorAll(`.${gridSquareClass}`);
+    gridSquares.forEach(square => square.addEventListener('mouseover', () => {
+        square.classList.add(activeClass);
+    }))
+}
+
+makeGrid(container, ROW_CLASS, GRID_SQUARE_CLASS);
+changeSquareColor(GRID_SQUARE_CLASS, ACTIVE_CLASS);
 console.log(container);
